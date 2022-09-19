7f7723e380037def290ebfcf5c8700f91f654c42
vvanglro <947001731@qq.com>
2022-09-19 15:19:58 +0800

    feat: install

--- build.py
+++ build.py
@@ -1,11 +1,16 @@
 import logging
 import subprocess
+from typing import Dict, Any
 
 
 def install_chromium():
     logging.info("\n")
     logging.info("Start downloading the chromium driver")
     subprocess.call("playwright install chromium", shell=True)
+
+
+def build(setup_kwargs: Dict[str, Any]) -> None:
+    install_chromium()
 
 
 if __name__ == "__main__":
